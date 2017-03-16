8018d3d66b0e8adb77c57b703ea55a2974cf226c
Jess Kane <jessplkane@gmail.com>
2017-03-16 10:39:41 +0000

    Update and rename JavaScript to JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -6,12 +6,12 @@ $(document).ready(function() {
 var url = "http://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=jsonp&lang=en&jsonp=?";
 var currentQuote = '';
 var currentAuthor = '';
-//Background images
+  //Background images
 var colors = ["https://www.houseofhackney.com/blog/wp-content/uploads/2013/12/palmeral-linen-white-bg2.jpg", "http://www.peopleofprint.com/wp-content/uploads/2014/10/hoh005.jpg", "http://www.peopleofprint.com/wp-content/uploads/2014/10/hoh001.jpg"];
-//Matching buttons
+  //Matching buttons
 var buttonColors = ["#435839", "#0E191F", "#CC1C31"];
 
-//Change background image
+  //Change background image
 var i = 0;
 
 function getColor(arr) { 
@@ -19,7 +19,7 @@ function getColor(arr) {
     $('#background').css('background-image', 'url('+ arr[i] +')');
 }
 
-//Change button colours
+  //Change button colours
 var count = 0
 
 function newColor(arr) {
@@ -28,10 +28,9 @@ function newColor(arr) {
   $('#getQuote').css('background',arr[count]);
 }
 
-//Get new quote
+  //Get new quote
 var getQuote = function(data) {
   currentQuote = $("#quoteBox").text("'" + data.quoteText + "'");
-  
   currentAuthor = $("#author").text("- " + data.quoteAuthor);
  
   // if author property has no value  
@@ -40,13 +39,11 @@ var getQuote = function(data) {
   }
   
   // pre-populate tweet with quote and author 
-  $('#twitter-share').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + data.quoteText + '" ' + data.quoteAuthor));
-  
+  $('#twitter-share').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + data.quoteText + '" ' + data.quoteAuthor));  
 }
 
 $("#getQuote").click(function() {
   $.getJSON(url, getQuote);
   getColor(colors);
   newColor(buttonColors);
-  
 });
